e5f6eab797dfd3ce8d6d1783b41c1d59fdcb2bba
AzizDevo <125346236+AzizDevo@users.noreply.github.com>
2023-03-09 01:33:31 +0400

    Project Added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp1.Models;
+
+Book[] Books = new Book[10];
+
+for (int i = 0; i < Books.Length; i++)
+{
+    Console.WriteLine("Book name : ");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Number of pages  : ");
+    int page = int.Parse(Console.ReadLine());
+
+    while (!CheckName(name))
+    {
+        Console.WriteLine("Please, enter your name correctly.");
+        name = Console.ReadLine();
+    }
+
+    while (!CheckCount(page))
+    {
+        Console.WriteLine("Please, enter the number of pages correctly");
+        page = int.Parse(Console.ReadLine());   
+    }
+    
+
+    Book book = new Book(name, page);
+    Books[i] = book;
+}
+
+bool CheckName(string name)
+{
+    if (!string.IsNullOrWhiteSpace(name) && name.Length > 2)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool CheckCount(int pageCount)
+{
+    if (pageCount > 10)
+    {
+        return true;
+    }
+    return false;
+}
+
+foreach (Book book in Books)
+{
+    Console.WriteLine($"Name : {book.Name}   pageCount : {book.PageCount}");
+}
